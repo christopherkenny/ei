e9d3d9735332e60b81d2a4872cfce10e28402383
Christopher Kenny <ctk44@cornell.edu>
2021-08-27 19:42:14 -0400

    enforce style guide

--- R/eiread.R
+++ R/eiread.R
@@ -1,6 +1,6 @@
-#@ei.object -- ei object
-#@... options
-#Extracts information from ei object
+# @ei.object -- ei object
+# @... options
+# Extracts information from ei object
 
 
 
@@ -49,35 +49,39 @@
 #'
 #' @examples
 #' data(sample)
-#' formula = t ~ x
-#' dbuf <- ei(formula=formula, total="n",data=sample)
+#' formula <- t ~ x
+#' dbuf <- ei(formula = formula, total = "n", data = sample)
 #' eiread(dbuf, "phi")
 #' eiread(dbuf, "betab", "betaw")
-#'
 #' @export eiread
-eiread <- function(ei.object, ...){
-  function.list <- list("betab" = .betaB, "betaw" = .betaW,
-                        "phi" = .phi, "sbetab" = .sbetab,
-                        "sbetaw" = .sbetaw, "psisims" = .psisims,
-                        "bounds" = .bounds, "CI80b" =.CI80b,
-                        "CI80w" = .CI80w, "abounds" = .abounds,
-                        "aggs" = .aggs, "maggs" = .maggs,
-                        "VCaggs" = .VCaggs, "eaggbias" = .eaggbias,
-                        "goodman" = .goodman)
+eiread <- function(ei.object, ...) {
+  function.list <- list(
+    "betab" = .betaB, "betaw" = .betaW,
+    "phi" = .phi, "sbetab" = .sbetab,
+    "sbetaw" = .sbetaw, "psisims" = .psisims,
+    "bounds" = .bounds, "CI80b" = .CI80b,
+    "CI80w" = .CI80w, "abounds" = .abounds,
+    "aggs" = .aggs, "maggs" = .maggs,
+    "VCaggs" = .VCaggs, "eaggbias" = .eaggbias,
+    "goodman" = .goodman
+  )
   dec <- ei.object$precision
   arguments <- list(...)
   results <- list()
   for (arg in arguments) {
-    if (arg %in% names(function.list))
+    if (arg %in% names(function.list)) {
       results[[arg]] <-
-        floor(function.list[[arg]](ei.object)*10^dec)/10^dec
-    else
+        floor(function.list[[arg]](ei.object) * 10^dec) / 10^dec
+    } else {
       results[[arg]] <- NA
+    }
   }
-  if (length(results)==1)
+  if (length(results) == 1) {
     results <- results[[1]]
-  if (length(results) <1)
+  }
+  if (length(results) < 1) {
     warning("qi results object is empty")
-  #class(results) <- "eiread"
+  }
+  # class(results) <- "eiread"
   results
 }
--- R/plot.R
+++ R/plot.R
@@ -1,5 +1,5 @@
-#@x - ei.object to plot
-#Plots in ei.
+# @x - ei.object to plot
+# Plots in ei.
 
 
 
@@ -80,42 +80,45 @@
 #'
 #' @examples
 #' data(sample)
-#' formula = t ~ x
-#' dbuf <- ei(formula=formula, total="n",data=sample)
+#' formula <- t ~ x
+#' dbuf <- ei(formula = formula, total = "n", data = sample)
 #' plot(dbuf, "tomog")
 #' plot(dbuf, "tomog", "betab", "betaw", "xtfit")
-#'
-plot.ei <- function(x, ...){
+plot.ei <- function(x, ...) {
   ei.object <- x
-  lci.function.list <- list("tomogD" = .tomog, "tomog"=.tomogl)    # Fix for passing lci value
-  function.list <- list("tomogCI"=.tomog80CI, "tomogCI95"=.tomog95CI,
-                        "tomogE"=.tomogE, "tomogP" = .tomogP2,
-                        "betab"=.betabd,
-                        "betaw"=.betawd, "xt"=.xt, "xtc"=.xtc,
-                        "xtfit"=.xtfit, "xtfitg"=.xtfitg,
-                        "estsims"=.estsims, "boundXb"=.boundXb,
-                        "boundXw"=.boundXw,
-                        "truth"=.truthfn,"eiRxCtomog"=.bndplot,
-                        "movieD"=.movieD, "movie"=.movie)
+  lci.function.list <- list("tomogD" = .tomog, "tomog" = .tomogl) # Fix for passing lci value
+  function.list <- list(
+    "tomogCI" = .tomog80CI, "tomogCI95" = .tomog95CI,
+    "tomogE" = .tomogE, "tomogP" = .tomogP2,
+    "betab" = .betabd,
+    "betaw" = .betawd, "xt" = .xt, "xtc" = .xtc,
+    "xtfit" = .xtfit, "xtfitg" = .xtfitg,
+    "estsims" = .estsims, "boundXb" = .boundXb,
+    "boundXw" = .boundXw,
+    "truth" = .truthfn, "eiRxCtomog" = .bndplot,
+    "movieD" = .movieD, "movie" = .movie
+  )
   arguments <- list(...)
   # Fix for passing lci value
-  if ("lci" %in% names(arguments)){
-    lci<-arguments$lci
-    arguments$lci<-NULL
+  if ("lci" %in% names(arguments)) {
+    lci <- arguments$lci
+    arguments$lci <- NULL
   } else {
-    lci<-TRUE
+    lci <- TRUE
   }
 
   results <- list()
-  if (length(arguments)!=1) {row = ceiling(length(arguments)/2)
-                             par(mfrow=c(row, 2))
-                           }
+  if (length(arguments) != 1) {
+    row <- ceiling(length(arguments) / 2)
+    par(mfrow = c(row, 2))
+  }
   for (arg in arguments) {
-    if (arg %in% names(function.list)){
-      results[[arg]] <- function.list[[arg]] (ei.object=ei.object)
+    if (arg %in% names(function.list)) {
+      results[[arg]] <- function.list[[arg]](ei.object = ei.object)
     } else if (arg %in% names(lci.function.list)) {
-      results[[arg]] <- lci.function.list[[arg]] (ei.object=ei.object, lci=lci)    # Fix for passing lci value
-    } else
+      results[[arg]] <- lci.function.list[[arg]](ei.object = ei.object, lci = lci) # Fix for passing lci value
+    } else {
       results[[arg]] <- NA
+    }
   }
 }
